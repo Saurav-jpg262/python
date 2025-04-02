b94a21c4c994631a1754b31dbdc179c03f452112
Saurav-jpg262 <kcsaurav010@gmail.com>
2025-04-02 10:06:40 +0000

    exercise list

--- exercise_list.py
+++ exercise_list.py
@@ -2,4 +2,5 @@ countries = ["Nepal","Korea","Japan","India"]
 print("second country:", countries[1])
 countries.append("China")
 removed_country = countries.pop(0)
-print("updated country list:", countries)
+for country in countries:
+    print(country)
