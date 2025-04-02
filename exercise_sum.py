8bd6b6d293166d1d0f4cbf1485e4d34410ecae81
Saurav-jpg262 <kcsaurav010@gmail.com>
2025-04-02 08:17:08 +0000

    exercise sum

---
+++ exercise_sum.py
@@ -0,0 +1,13 @@
+variable_total = 0
+data = 0
+while True:
+    data = int(input("Enter the number:  "))
+    if data != 0:
+        variable_total = variable_total + data
+        
+    else:
+         print("The programe end")
+         break
+    
+print(f"The total is {variable_total}")
+
