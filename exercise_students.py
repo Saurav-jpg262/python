ef201135c083fb046d2d02aa545023739cc64a55
Saurav-jpg262 <kcsaurav010@gmail.com>
2025-04-03 09:51:50 +0000

    exercise distionary

---
+++ exercise_students.py
@@ -0,0 +1,9 @@
+person = {
+    "first_name":  "sandy",
+    "last_name": "randi",
+    "age": "30",
+}
+
+print("first_name: ", person["first_name"])
+print("last_name: ", person["last_name"])
+print("age: ", person["age"])
