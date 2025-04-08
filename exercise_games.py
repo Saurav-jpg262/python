2c7612fa83d8dd583c2ef9d28f03be4bd396709a
Saurav-jpg262 <kcsaurav010@gmail.com>
2025-04-08 15:27:12 +0000

    on a way

---
+++ exercise_games.py
@@ -0,0 +1,13 @@
+games = [
+    {"name": "GG", "editor": "randi", "year": 2018}
+]
+
+new_game = {
+    "name": "ff",
+    "editor": "sandy",
+    "year": 2019
+}
+games.append(new_game)
+
+for game in games:
+   print(f"name")
