4c9384b2d607ed7e4853ec81017ff111ff3f8624
Saurav-jpg262 <kcsaurav010@gmail.com>
2025-04-09 09:54:47 +0000

    something missed

--- exercise_shop.py
+++ exercise_shop.py
@@ -4,10 +4,25 @@ products = [
 {"name": "nokia", "price": 700},
 ]
 
-career = [
+careers = [
     {"company_name": "pathao", "fees": 25},
     {"company_name": "indrive", "fees": 20},
     {"company_name": "foodmandu", "fees": 15},
 ]
 
-print("Available products")
+for index, career in enumerate(careers):
+    print(f"For {career["name"]} press {index}")
+while True:
+    user_career_index = input("Please enter the product number:\n")
+    try:
+        user_career_index = int(user_career_index)
+    
+        if user_career_index >=0 and user_career_index < len(careers):
+            break
+        else:
+            print("This product does not exit")
+    
+    except ValueError:
+        print("Please only enter number")
+
+user_career = careers[user_career_index]
