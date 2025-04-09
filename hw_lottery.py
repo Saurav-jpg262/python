e803b2ca36c8d2527852197593cda7468fab698f
Saurav-jpg262 <kcsaurav010@gmail.com>
2025-04-09 05:04:28 +0000

    test

--- hw_lottery.py
+++ hw_lottery.py
@@ -1,15 +1,34 @@
-user_numbers = set()
-while True:
-    if len(user_numbers) >= 7:
-        break
-    try:
-        user_input = int(input(f"Enter a number {len(user_numbers)+1}/7 : \n"))
-        if user_input in user_numbers:
-            print("You already entered that number")
-        else:
-            user_numbers.add(user_input)
-    except ValueError:
-        print("Please enter only numbers")
-print("Your lottery numbers are:")
-for number in user_numbers:
-    print(number)
+numbers = set()
+
+while len(numbers) < 7:
+    number = int(input("Enter a number:\n "))
+    
+
+    if number in numbers:
+        print("You already entered this number. Try a different one.")
+    else:
+        numbers.add(number)
+
+print("Your numbers are:", numbers)
+
+winning_numbers = {10, 25, 32, 41, 43, 45, 50}
+
+
+correct = numbers & winning_numbers  
+count = len(correct)
+
+
+if count == 3:
+    print("You win $4")
+elif count == 4:
+    print("You win $15")
+elif count == 5:
+    print("You win $200")
+elif count == 6:
+    print("You win $30000")
+elif count == 7:
+    print("You win $5,000,000")
+else:
+    print("Sorry, no prize.")
+
+print("Correct numbers:"correct)
