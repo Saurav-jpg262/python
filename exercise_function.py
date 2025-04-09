2f55561836b50d3dc1f3ce80c8301bf9c47274dd
Saurav-jpg262 <kcsaurav010@gmail.com>
2025-04-09 10:07:22 +0000

    complete

---
+++ exercise_function.py
@@ -0,0 +1,7 @@
+first_name = input("Please enter your first name:  ")
+last_name = input("Please enter your last name:  ")
+
+def display_full_name(first_name, last_name):
+    print(first_name, last_name)
+
+display_full_name(first_name, last_name)
