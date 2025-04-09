5447bfc6ce3779032c46ec5cc677dc78535016f9
Saurav-jpg262 <kcsaurav010@gmail.com>
2025-04-09 09:12:20 +0000

    correction

---
+++ exercise_shop.py
@@ -0,0 +1,13 @@
+products = [
+{"name": "iphone", "price": 1400},
+{"name": "samsung", "price": 900},
+{"name": "nokia", "price": 700},
+]
+
+career = [
+    {"company_name": "pathao", "fees": 25},
+    {"company_name": "indrive", "fees": 20},
+    {"company_name": "foodmandu", "fees": 15},
+]
+
+print("Available products")
